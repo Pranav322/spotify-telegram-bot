4fe5331143df055c2c430c42e6438765e7ca5e8c
Pranav kumar <pranav88096@gmail.com>
2024-03-28 02:07:17 +0530

    chnged urls for merged playlist views

--- bot/urls.py
+++ bot/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import spotify_callback , check_user_id , get_user_playlists
+from .views import spotify_callback , check_user_id , get_user_playlists , merge_playlists
 
 urlpatterns = [
   
     path('callback/' , spotify_callback , name = "callback"),
     path('check/' , check_user_id , name = "check_user"),
-    path('playlist/' , get_user_playlists , name = "user_playlists")
+    path('playlist/' , get_user_playlists , name = "user_playlists"),
+    path('mergeplaylists/' , merge_playlists , name = "merge_playlist")
     # Add other URL patterns as needed
 ]
